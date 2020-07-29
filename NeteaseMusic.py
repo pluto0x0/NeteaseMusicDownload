52e18c1b278abee0bf2a7b237cf5607a165a180e
pluto0x0 <ying.zf@foxmail.com>
2020-07-29 22:43:58 +0800

    mutagen added

--- NeteaseMusic.py
+++ NeteaseMusic.py
@@ -10,8 +10,12 @@ import os
 import platform
 from configparser import ConfigParser
 import hashlib
+import sys
 
+# 配置文件名
 confName = 'NeteaseMusic.conf'
+if len(sys.argv) >= 2:
+    confName = sys.argv[2]
 # 检测OS类型
 isWindows = (re.search('windows', platform.architecture()[1], re.I) != None)
 
@@ -102,7 +106,8 @@ def getDetail(songids):
         if res.status_code == 200 and res.json()['code'] == 200:
             data = res.json()['songs'][0]
             song['name'] = data['name']
-            song['time'] = data['publishTime']
+            song['year'] = str(
+                time.localtime(data['publishTime'] / 1000).tm_year)
             song['album'] = data['al']['name']
             song['pic'] = data['al']['picUrl']
             song['artists'] = []
@@ -139,10 +144,26 @@ def getDetail(songids):
         if res.status_code == 200 and res.json()['code'] == 200:
             data = res.json()['data'][0]
             song['url'] = data['url']
-            song['type'] = data['type']
+            if song['url'] != None:
+                song['type'] = data['type'].lower()
             if song['url'] != None or config['download'][
                     'skipDisabled'] != 'True':
+                # 文件名
+                conf = {}
+                conf['year'] = song['year']
+                conf['name'] = fileStr(song['name'])
+                conf['album'] = fileStr(song['album'])
+                conf['artist'] = fileStr(','.join(song['artists']))
+                conf['index'] = i + 1
+                song['filename'] = ''
+                if config['download']['format'] == 'format':
+                    song['filename'] = FormateStr.format(**conf)
+                else:
+                    # 危
+                    song['filename'] = eval(FormateStr)
+
                 songs.append(song)
+
             print('完成。')
         else:
             log('音乐' + str(song['id']) + 'url错误' + str(res.status_code) +
@@ -213,24 +234,14 @@ input('按下回车开始下载…')
 # 下载
 for i in range(len(songs)):
     song = songs[i]
-    conf = {}
-    conf['name'] = fileStr(song['name'])
-    conf['album'] = fileStr(song['album'])
-    conf['artist'] = fileStr(','.join(song['artists']))
-    conf['index'] = i + 1
-    fname = ''
-    if config['download']['format'] == 'format':
-        fname = FormateStr.format(**conf)
-    else:
-        # 危
-        fname = eval(FormateStr)
+    fname = song['filename']
     # 清屏
     os.system('cls' if isWindows else 'clear')
     # 显示进度
     print('({:4}/{:4}) {:.2f}%'.format(i + 1, len(songs),
                                        100 * (i + 1) / len(songs)))
 
-    if config['download']['getPic'] == 'True':
+    if config['download']['getPic'] == 'True' or config['tags']['writeCover']:
         # 下载封面
         url = song['pic']
         filename = fname + getExt(url)
@@ -289,7 +300,63 @@ for i in range(len(songs)):
                 res = requests.get(url)
                 with open(Dirname + '/' + filename, 'wb') as file:
                     file.write(res.content)
-    time.sleep(0.1)
+    # time.sleep(0.1)
 
-exit()
-# TODO: mutagen
+if config['tags']['writeTags'] != 'True':
+    exit()
+
+# 文档：https://mutagen.readthedocs.io/
+import mutagen.flac, mutagen.id3
+
+for song in songs:
+    if song['url'] == None:
+        continue
+    filename = Dirname + '/' + song['filename']
+    artist = fileStr(','.join(song['artists']))
+    try:
+        if song['type'] == 'mp3':
+            tag = mutagen.id3.ID3()
+            img = open(filename + '.jpg', 'rb')
+            if config['tags']['ID3v2x'] == '3':
+                tag.update_to_v23()
+            tag['APIC'] = mutagen.id3.APIC(  #插入专辑图片
+                encoding=0,  # ass hole, for sony A35
+                mime=u'image/jpeg',
+                type=mutagen.id3.PictureType.COVER_FRONT,
+                desc='Cover',
+                data=img.read())
+            tag['TPE1'] = mutagen.id3.TPE1(  #插入第一演奏家、歌手、等
+                encoding=3, text=[artist])
+            tag['TALB'] = mutagen.id3.TALB(  #插入专辑名称
+                encoding=3, text=[song['album']])
+            tag['TIT2'] = mutagen.id3.TIT2(  #插入歌名
+                encoding=3, text=[song['name']])
+            tag['TYER'] = mutagen.id3.TYER(  #插入专辑名称
+                encoding=3, text=[song['year']])
+            v2x = int(config['tags']['ID3v2x'])
+            tag.save(filename + '.mp3',
+                    v1=int(config['tags']['WriteID1']),
+                    v2_version=v2x)
+            img.close()
+        elif song['type'] == 'flac':
+            audio = mutagen.flac.FLAC(filename + '.flac')
+            audio.delete()
+            audio['title'] = song['name']
+            audio['album'] = song['album']
+            audio['artist'] = artist
+            audio['date'] = song['year']
+
+            img = mutagen.flac.Picture()
+            with open(filename + '.jpg', 'rb') as fil:
+                img.data = fil.read()
+            img.type = mutagen.id3.PictureType.COVER_FRONT
+            img.mime = u"image/jpeg"
+            img.desc = u'Cover'
+
+            audio.add_picture(img)
+            audio.save()
+        else:
+            print('未知数据格式')
+        print('写入{0}完成'.format(song['name']))
+    except BaseException:
+        print('error on' + song['name'])
